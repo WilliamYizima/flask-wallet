d36331caaa5694ff21394620763615e3d063349d
will <william.yizima@hotmail.com>
2021-07-11 23:51:34 -0300

    create db to receipts and migration

--- flask_wallet/app.py
+++ flask_wallet/app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 
+from flask_wallet.ext import config
 from flask_wallet.ext import site
+from flask_wallet.ext import db
+from flask_wallet.ext import migrate
 from flask_wallet.ext import cli
 
 def create_app():
     app = Flask(__name__)
 
+    config.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app)
     cli.init_app(app)
     site.init_app(app)
-    
+
     return app
--- flask_wallet/ext/cli/__init__.py
+++ flask_wallet/ext/cli/__init__.py
@@ -1,4 +1,5 @@
 import click
+from flask_wallet.ext.db import db
 
 def init_app(app):
 
@@ -6,3 +7,10 @@ def init_app(app):
     def hello():
         """Comando teste do CLI"""
         click.echo("Olá! o Teste rolou!")
+
+    @app.cli.command()
+    def create_db():
+        """Comando teste do CLI"""
+        db.create_all()
+
+        click.echo("Criei as tabelas necessárias")
