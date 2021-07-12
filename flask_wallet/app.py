f630a9af3c8f183058335242f433814d12419222
will <william.yizima@hotmail.com>
2021-07-12 00:46:18 -0300

    insert form

--- flask_wallet/app.py
+++ flask_wallet/app.py
@@ -5,6 +5,7 @@ from flask_wallet.ext import site
 from flask_wallet.ext import db
 from flask_wallet.ext import migrate
 from flask_wallet.ext import cli
+from flask_wallet.ext import api
 
 def create_app():
     app = Flask(__name__)
@@ -13,6 +14,7 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app)
     cli.init_app(app)
+    api.init_app(app)
     site.init_app(app)
 
     return app
